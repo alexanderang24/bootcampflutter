0077d9cdbd240602efcf852ddf9299128ec59556
Alexander Ang <alexanderang.24@gmail.com>
2021-05-18 23:14:21 +0800

    tugas 12: flutter component

---
+++ sanberappflutter/lib/Tugas/Tugas12/Models/Chart_model.dart
@@ -0,0 +1,89 @@
+class ChartModel {
+  final String name;
+  final String message;
+  final String time;
+  final String profileUrl;
+
+  ChartModel({this.name, this.message, this.time, this.profileUrl});
+}
+
+final List<ChartModel> items = [
+  ChartModel(
+      name: 'hilmy',
+      message: 'Hello Hilmy',
+      time: '12.00',
+      profileUrl: 'https://asset.kompas.com/crops/IjmmRwybxe-4tZrGRN5eMrq_po8=/0x32:700x382/750x500/data/photo/2020/01/09/5e16811892fc7.jpg'),
+  ChartModel(
+      name: 'riska',
+      message: 'hello riska',
+      time: '9 march',
+      profileUrl: 'https://asset.kompas.com/crops/IjmmRwybxe-4tZrGRN5eMrq_po8=/0x32:700x382/750x500/data/photo/2020/01/09/5e16811892fc7.jpg'),
+  ChartModel(
+      name: 'vita',
+      message: 'hello vita',
+      time: '10 march',
+      profileUrl: 'https://asset.kompas.com/crops/IjmmRwybxe-4tZrGRN5eMrq_po8=/0x32:700x382/750x500/data/photo/2020/01/09/5e16811892fc7.jpg'),
+  ChartModel(
+      name: 'niva',
+      message: 'hello niva',
+      time: '12.21',
+      profileUrl:
+          'https://asset.kompas.com/crops/ployX7cQOqsYqJS2PYvUGv41CaI=/0x0:1000x667/750x500/data/photo/2017/06/22/163146320170622-042902-8311-chef.juna-.atau-.junior-.rorimpandey-.jpg'),
+  ChartModel(
+      name: 'citra',
+      message: 'hello citra',
+      time: '13.41',
+      profileUrl:
+          'https://asset.kompas.com/crops/ployX7cQOqsYqJS2PYvUGv41CaI=/0x0:1000x667/750x500/data/photo/2017/06/22/163146320170622-042902-8311-chef.juna-.atau-.junior-.rorimpandey-.jpg'),
+  ChartModel(
+      name: 'adi',
+      message: 'hello adi',
+      time: '25 january',
+      profileUrl:
+          'https://asset.kompas.com/crops/ployX7cQOqsYqJS2PYvUGv41CaI=/0x0:1000x667/750x500/data/photo/2017/06/22/163146320170622-042902-8311-chef.juna-.atau-.junior-.rorimpandey-.jpg'),
+  ChartModel(
+      name: 'ashraf',
+      message: 'hello ashraf',
+      time: '19.00',
+      profileUrl: 'https://asset.kompas.com/crops/8ywqoPkrrT2h4Faw3v0nE3an9-g=/0x0:1003x669/750x500/data/photo/2020/10/20/5f8ea23458b38.jpg'),
+  ChartModel(
+      name: 'niva',
+      message: 'hello niva',
+      time: '12.21',
+      profileUrl: 'https://asset.kompas.com/crops/8ywqoPkrrT2h4Faw3v0nE3an9-g=/0x0:1003x669/750x500/data/photo/2020/10/20/5f8ea23458b38.jpg'),
+  ChartModel(
+      name: 'citra',
+      message: 'hello citra',
+      time: '13.41',
+      profileUrl: 'https://asset.kompas.com/crops/8ywqoPkrrT2h4Faw3v0nE3an9-g=/0x0:1003x669/750x500/data/photo/2020/10/20/5f8ea23458b38.jpg'),
+  ChartModel(
+      name: 'adi',
+      message: 'hello adi',
+      time: '25 january',
+      profileUrl: 'https://asset.kompas.com/crops/qTMx96STZNVw_HS8pCP4VT8wYU0=/91x8:1090x673/750x500/data/photo/2020/12/28/5fe9c4b788cd8.jpg'),
+  ChartModel(
+      name: 'ashraf',
+      message: 'hello ashraf',
+      time: '19.00',
+      profileUrl: 'https://asset.kompas.com/crops/qTMx96STZNVw_HS8pCP4VT8wYU0=/91x8:1090x673/750x500/data/photo/2020/12/28/5fe9c4b788cd8.jpg'),
+  ChartModel(
+      name: 'niva',
+      message: 'hello niva',
+      time: '12.21',
+      profileUrl: 'https://asset.kompas.com/crops/qTMx96STZNVw_HS8pCP4VT8wYU0=/91x8:1090x673/750x500/data/photo/2020/12/28/5fe9c4b788cd8.jpg'),
+  ChartModel(
+      name: 'citra',
+      message: 'hello citra',
+      time: '13.41',
+      profileUrl: 'https://asset.kompas.com/crops/C_dSrAyZaTB41qUJ3g16Os61-Po=/389x160:863x476/750x500/data/photo/2021/02/15/602a138009d03.png'),
+  ChartModel(
+      name: 'adi',
+      message: 'hello adi',
+      time: '25 january',
+      profileUrl: 'https://asset.kompas.com/crops/C_dSrAyZaTB41qUJ3g16Os61-Po=/389x160:863x476/750x500/data/photo/2021/02/15/602a138009d03.png'),
+  ChartModel(
+      name: 'ashraf',
+      message: 'hello ashraf',
+      time: '19.00',
+      profileUrl: 'https://asset.kompas.com/crops/C_dSrAyZaTB41qUJ3g16Os61-Po=/389x160:863x476/750x500/data/photo/2021/02/15/602a138009d03.png'),
+];
